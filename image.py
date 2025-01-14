f9e34564d34a9f8535fbf2063d98a5d6604c449c
Arian <ariannasiri86@gmail.com>
2025-01-14 23:45:55 +0330

    Add "create ASCII image" feature for Image class

--- image.py
+++ image.py
@@ -1,4 +1,7 @@
-from PIL import Image as Img
+from pathlib import Path
+from datetime import datetime
+
+from PIL import Image as Img, ImageDraw, ImageFont
 
 
 class Image:
@@ -40,3 +43,25 @@ class Image:
             rows.append(row)
 
         return '\n'.join(rows)
+
+    def create_ascii_image(self, image_path: Path, chars: str, background_color: str):
+        """Create an image from ASCII"""
+
+        one_char_width = 8  # Width of each character in image
+        one_char_height = 18  # Height of each character in image
+        font = ImageFont.truetype('arial.ttf', 15)
+        pixels = self.resize_image().load()  # Get image pixels
+        width, height = self.image_size
+        output_image = Img.new('RGB', (one_char_width * width, one_char_height * height),
+                               color=background_color)  # Create an empty image frame with image and char size
+        d = ImageDraw.Draw(output_image)
+
+        # Draw ASCII characters to the image frame
+        for y in range(height):
+            for x in range(width):
+                r, g, b = pixels[x, y]
+                h = int(r/3 + g/3 + b/3)
+                char = chars[((h * len(chars)) // 255) - 1]
+                d.text((x * one_char_width, y * one_char_height), char, font=font, fill=(r, g, b))
+
+        output_image.save(image_path / f'ascii image {datetime.now().strftime("%Y-%m-%d %H%M%S")}.png')  # Save image
